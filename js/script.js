94a2dd2759970e5de6d9224f07cb070a44c49fd8
Asa J McClellan <56137943+asamcclellan@users.noreply.github.com>
2021-05-15 13:31:47 -0500

    word shows at 0 guesses remaining, game refreshes after win state

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const playAgainButton = document.querySelector(".play-again")
 // hidden: appears when it's time to play again
 let word = "";
 // starting word to test the script
-const guessedLetters = [];
+let guessedLetters = [];
 // this will contain the players guesses
 let remainingGuesses = 8;
 // maximum number of guesses the player can make
@@ -33,7 +33,7 @@ const getWord = async function () {
     console.log(`Random word: ${randomWord}`);
     word = randomWord;
 
-    const freshWord = [];
+    let freshWord = [];
 
     for (let letter of word) {
         freshWord.push("●");
@@ -76,9 +76,9 @@ const checkInput = function (inputValue) {
                     guessedLetters.push(inputValue);
                     // console.log(guessedLetters);
                     displayGuesses(guessedLetters);
+                    updateWip(guessedLetters);
                     guessCountdown(inputValue);
 
-                    updateWip(guessedLetters);
                     return inputValue;
                 } else {
                     message.innerText = `You already guessed ${inputValue}!`;
@@ -134,6 +134,9 @@ const checkIfWin = function () {
     if (word.toUpperCase() === progress.innerText) {
         message.classList.add("win");
         message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+        startOver();
+    } else {
+
     }
 }
 
@@ -146,6 +149,43 @@ const guessCountdown = function (inputValue) {
     } else {
         message.innerText = "That guess wasn't part of the word. :/";
         remainingGuesses -= 1;
-        remainingSpan.innerText = `${remainingGuesses} guesses`
+        remainingSpan.innerText = `${remainingGuesses} guesses`;
+        if (remainingGuesses === 0) {
+            message.innerText = "You ran out of guesses. Try again?";
+            progress.innerText = word.toUpperCase();
+            startOver();
+        }
     }
 }
+
+const startOver = function () {
+    guessButton.classList.add("hide");
+    remaining.classList.add("hide");
+    guesses.classList.add("hide");
+    playAgainButton.classList.remove("hide");
+    remainingGuesses = 8;
+    guessedLetters = [];
+}
+
+playAgainButton.addEventListener("click", function () {
+    if (message.classList.contains("win")) {
+        message.classList.remove("win");
+        message.innerHTML = "";
+        guessButton.classList.remove("hide");
+        remaining.classList.remove("hide");
+        guesses.classList.remove("hide");
+        playAgainButton.classList.add("hide");
+        remainingSpan.innerText = `${remainingGuesses} guesses`;
+        guesses.innerText = "";
+        getWord();
+    } else {
+        guessButton.classList.remove("hide");
+        remaining.classList.remove("hide");
+        guesses.classList.remove("hide");
+        playAgainButton.classList.add("hide");
+        guesses.innerText = "";
+        message.innerHTML = "";
+        remainingSpan.innerText = `${remainingGuesses} guesses`;
+        getWord();
+    }
+})
